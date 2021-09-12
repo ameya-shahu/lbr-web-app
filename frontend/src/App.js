fa0b8c26aa7b275b5735bf443cc848874a9b4060
SohanR09 <sohan1203@gmail.com>
2021-09-12 20:53:14 +0530

    file structure

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <div className='container'>
+        <nav class="navbar navbar-light bg-light">
+          <span class="navbar-brand mb-0 p-2 h1">Testing</span>
+        </nav>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
